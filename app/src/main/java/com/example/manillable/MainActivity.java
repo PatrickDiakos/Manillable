60290c384712c96aa5918bdfa0794dbeccf971fb
PatrickDiakos <90656772+PatrickDiakos@users.noreply.github.com>
2022-07-09 17:42:28 +1000

    VCS Test - Ownership

--- app/src/main/java/com/example/manillable/MainActivity.java
+++ app/src/main/java/com/example/manillable/MainActivity.java
@@ -11,5 +11,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //testing commit #2
+        //testing new ownership of VCS
     }
 }
